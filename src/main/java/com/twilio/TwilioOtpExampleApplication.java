d2117ba85a498dd5942527e6da310e8f94cb6284
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-07-27 01:16:20 +0300

    Add files via upload

---
+++ src/main/java/com/twilio/TwilioOtpExampleApplication.java
@@ -0,0 +1,12 @@
+package com.twilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioOtpExampleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioOtpExampleApplication.class, args);
+	}
+
+}
